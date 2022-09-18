1456aafe36c4b1939b173de7cceb834d51b2c027
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-09-18 18:47:12 +0200

    ✨ Add progress bar and example .env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import { ChakraProvider } from '@chakra-ui/react';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <ChakraProvider>
+            <NextNProgress
+                color="#29D"
+                startPosition={0.15}
+                stopDelayMs={200}
+                height={4}
+                options={{ showSpinner: false }}
+            />
             <Component {...pageProps} />
         </ChakraProvider>
     );
